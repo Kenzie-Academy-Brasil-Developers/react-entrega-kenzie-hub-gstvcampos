2f4cd631f345ae263056637648f5ff122637cdfd
gstvcampos <gustavo.bmc76@gmail.com>
2023-06-14 16:01:38 -0300

    paginas criadas

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import RountesMain from './routes/RountesMain'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      
-    </>
+    <div className=''>
+      <RountesMain/>
+    </div>
   )
 }
 
